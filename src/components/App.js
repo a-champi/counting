0edcdda7bfde8a96af7b9991cbfd3934523bbd2e
a-champi <a.champi.h@gmail.com>
2022-07-24 15:25:17 +0200

    istal react && set component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+//import React, {Component} from 'react';
+import React from 'react';
+import './App.css';
+import Count from "./Count";
+
+const  App = () => {
+  return (
+    <div className="App">          
+      <Count />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Count.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./App.css";
+import "../style/count.css";
+
+const Count = () => {
+  const [count, setCount] = useState(5);
+
+  // function for adding
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  //funtion for minus
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  //funtion reset
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter">
+      <h2>Counting Maschine</h2>
+      <div className="display_counter">
+        <h3>{count}</h3>
+      </div>
+      <div className="display_add_minus">
+        <button className="add" onClick={increase}>+
+        </button>
+        <button className="minus" onClick={decrease}>-
+        </button>
+      </div>
+      <div className="display_reset">
+        <button className="reset" onClick={reset}>reset
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Count;
